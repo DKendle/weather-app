665dfa02a073a8e7f07904baceb2596261f42d67
DKendle <dina.kendle@gmail.com>
2022-09-27 16:43:28 -0400

    Adds sample Table and data for component creation

---
+++ weather/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Table from "./Table"
+
+class App extends Component {
+    render() {
+      return (
+        <div className="App">
+          <h1>Weather Monitor</h1>
+          <Table />
+        </div>
+      )
+    }
+  }
+
+
+export default App
