97c64b13e6c07bb1c1066eaea7abc089af07dd63
DKendle <dina.kendle@gmail.com>
2022-09-29 18:06:10 -0400

    Adds component to pull API data for pokemon

---
+++ weather/src/Info.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { useEffect, useState } from "react";
+
+
+const [pokemon, setPokemon] = useState([])
+
+
+function Info(){
+
+
+    return(
+        <div>
+        </div>
+    )
+
+
+}
