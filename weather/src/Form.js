d5b031bad27ea3837e9a7267eac12f19c734b87b
DKendle <dina.kendle@gmail.com>
2022-09-30 13:36:58 -0400

    Adds form input handler to capture text in state

--- weather/src/Form.js
+++ weather/src/Form.js
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useState} from "react";
 
-
-const handleSubmit = () => {
-
-}
 
 function Form(){
 
+
+    const [input, setInput] = useState([])
+
+    const handleSubmit = () => {
+
+    }
+    
+    const handleInput = (e) => {
+        setInput(e.target.value)
+        console.log(input)
+    }
+        
     return(
        <form>
         <label>
             Enter Pokemon:
-            <input type="text" />
+            <input onChange={handleInput} type="text" value={input}/>
         </label>
         <input type="submit" value="Submit"/>
        </form>
