5265943edfd3696f34c32f36de04765474b7fb8c
DKendle <dina.kendle@gmail.com>
2022-10-09 17:34:22 -0400

    Changes api url to reusable variable

--- weather/src/Info.js
+++ weather/src/Info.js
@@ -10,9 +10,10 @@ function Info(){
     const listedPokes = pokemon.map(p => <li>{p.name}</li>)
     const pokeCards = pokemon.map(p => <PokemonCard name={p.name} />
     )
+    const url = "https://pokeapi.co/api/v2/pokemon/"
     
     useEffect(()=> {
-        fetch("https://pokeapi.co/api/v2/pokemon/")
+        fetch(url)
         .then(response => response.json())
         .then(data => setPokemon(data.results))
     }, [])
